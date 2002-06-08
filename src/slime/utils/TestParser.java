af7a1cb335e7e5449d2f0b116d5b03ee049ca229
swprakt <msteffen@ifi.uio.no>
2002-06-08 16:12:28 +0000

    forte test

--- src/slime/utils/TestParser.java
+++ src/slime/utils/TestParser.java
@@ -1,6 +1,4 @@
-
 package utils;
-
 
 import java.io.IOException;
 import java.io.*;
@@ -8,42 +6,38 @@ import java.util.*;
 import absynt.*;
 
 
-/**
- * Standalone file to test the parser on input files
+/** Standalone file to test the parser on input files
  * @author Martin Steffen, Karsten Stahl
- * @version $Id: TestParser.java,v 1.1 2002-06-07 15:04:19 swprakt Exp $
+ * @version $Id: TestParser.java,v 1.2 2002-06-08 16:12:28 swprakt Exp $
  *
  * ============== //-->
  */
 public class TestParser {
-  public static void main (String args []) {
-    try{
-      utils.Parser parser = new utils.Parser();
-      PrettyPrint pp = new PrettyPrint();
-      System.out.println("Input SAP program:");
-      String _sap = new String();
-      BufferedReader in
-	  = new BufferedReader(new InputStreamReader(System.in));
-      _sap = in.readLine();
-      System.out.println("parse: " + _sap);
-      LinkedList sap = parser.parseSAP(_sap);
-      System.out.println("*** Test: pretty print the result of parsing ***");
-      pp.printSAP(sap);
-    } catch (ParseException e){
-      System.err.println(e.toString());
-      System.exit(1);
-    } catch (IOException e){
-      System.err.println(e.toString());
-      System.exit(1);
-    } catch (Exception e){
-      System.err.println(e.toString());
-      System.exit(1);
+    
+    /** main method to call from commandline
+     * @param args from commandline
+     */
+    public static void main(String args []) {
+        try{
+            utils.Parser parser = new utils.Parser();
+            PrettyPrint pp = new PrettyPrint();
+            System.out.println("Input SAP program:");
+            String _sap = new String();
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            _sap = in.readLine();
+            System.out.println("parse: " + _sap);
+            LinkedList sap = parser.parseSAP(_sap);
+            System.out.println("*** Test: pretty print the result of parsing ***");
+            pp.printSAP(sap);
+        } catch (ParseException e){
+            System.err.println(e.toString());
+            System.exit(1);
+        } catch (IOException e){
+            System.err.println(e.toString());
+            System.exit(1);
+        } catch (Exception e){
+            System.err.println(e.toString());
+            System.exit(1);
+        }
     }
-  }
 }
-
-
-
-
-
-
