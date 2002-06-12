5d1a579129040ca7a599282c556774f8eeb63980
swprakt <msteffen@ifi.uio.no>
2002-06-12 10:07:57 +0000

    *** empty log message ***

--- src/slime/checks/IChecks.java
+++ src/slime/checks/IChecks.java
@@ -6,9 +6,15 @@ import absynt.*;
 /**
  * Top-level interface for the static analysis part of the
  * Slime tool.
+ *
+ * Currently, only _checking_ is supported, there is no attempt
+ * to derive omitted type information. Therefore, the return-type
+ * of the methods is boolean. The convention is that the a return
+ * value of true indicates success, a failure is indicated by
+ * an exception. Int other woirds: false is never returned.
 
  * @author Initially provided by Karsten Stahl, Martin Steffen.
- * @version  $Id: IChecks.java,v 1.4 2002-06-12 07:41:10 swprakt Exp $
+ * @version  $Id: IChecks.java,v 1.5 2002-06-12 10:07:57 swprakt Exp $
  */
 
 
@@ -24,6 +30,11 @@ public interface IChecks{
 //	----------------------------------------
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.4  2002/06/12 07:41:10  swprakt
+//	Top-level/general Exceptions added to the interface.
+//	
+//	M. Steffen
+//	
 //	Revision 1.3  2002/06/12 07:31:02  swprakt
 //	minimalistic interface (no exceptions given as yet),
 //	
