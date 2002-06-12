af5e0c07cb6743947abce394051c00d25d703c1d
swprakt <msteffen@ifi.uio.no>
2002-06-12 19:37:04 +0000

    Commentary in English

--- src/slime/utils/PpExample.java
+++ src/slime/utils/PpExample.java
@@ -7,9 +7,9 @@ import java.util.*;
 
 
 /**
-   Beispiel fuer die Verwendung des Pretty Printers.
+   Example for the usage of the pretty printer
  * @author Initially provided by Martin Steffen.
- * @version  $Id: PpExample.java,v 1.3 2002-06-12 18:52:07 swprakt Exp $    
+ * @version  $Id: PpExample.java,v 1.4 2002-06-12 19:37:04 swprakt Exp $    
  */
 
 
@@ -33,9 +33,20 @@ public class PpExample {
 //	Example for the pretty printer
 //	------------------------------
 //
-//	$Id: PpExample.java,v 1.3 2002-06-12 18:52:07 swprakt Exp $
+//	$Id: PpExample.java,v 1.4 2002-06-12 19:37:04 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.3  2002/06/12 18:52:07  swprakt
+//	reorganization of the package-structure
+//	
+//		src/<package>  => src/slime/<package>
+//	
+//	
+//	as decided in the group meeting. consequently, some adaption had to be done
+//	(wrt. import, package name etc). It compiles again.
+//	
+//	[M. Steffen]
+//	
 //	Revision 1.2  2002/06/07 15:04:17  swprakt
 //	added parser for SAP programs, added function printSAP() to PP
 //	
