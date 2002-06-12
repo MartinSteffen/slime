565dab552f1949983e86782ccc0c0795d92a49a8
swprakt <msteffen@ifi.uio.no>
2002-06-12 09:20:03 +0000

    cosmetics (M. Steffen)

--- src/slime/checks/Checks.java
+++ src/slime/checks/Checks.java
@@ -7,17 +7,19 @@ import absynt.*;
  * Top-level class providing the functionality for  the static analysis
  * par for  Slime.
  * @author Initially provided by Karsten Stahl, Martin Steffen.
- * @version  $Id: Checks.java,v 1.1 2002-06-12 09:18:56 swprakt Exp $
+ * @version  $Id: Checks.java,v 1.2 2002-06-12 09:20:03 swprakt Exp $
  */
 
 
 
 public class Checks implements IChecks {
   public boolean isWellTyped(SFC p) throws CheckException {
-    return true;}
+    return true;
+  }
 
   public boolean isWellformed(SFC p) throws CheckException{
-    return true;}
+    return true;
+  }
 }
 
 
@@ -27,5 +29,11 @@ public class Checks implements IChecks {
 //	----------------------------------------
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.1  2002/06/12 09:18:56  swprakt
+//	First version: compilable stub code. The checks do nothing, they
+//	optimistically return true, no exceptions.
+//	
+//	M. Steffen
+//	
 //	
 //---------------------------------------------------------------------
