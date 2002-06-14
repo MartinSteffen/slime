d6db232be26937340e40575aa6342f158ab0eb15
swprakt <msteffen@ifi.uio.no>
2002-06-14 09:22:52 +0000

    *** empty log message ***

---
+++ src/slime/checks/Typeerrors.java
@@ -0,0 +1,29 @@
+package slime.checks;
+import java.lang.*;
+import slime.absynt.*;
+
+
+/**
+ * Exceptions to be thrown by the type checker.
+ * The type checker's exceptions are inner classes of
+ * this class.
+
+ * @author Initially provided by Karsten Stahl, Martin Steffen.
+ * @version  $Id: Typeerrors.java,v 1.1 2002-06-14 09:22:52 swprakt Exp $
+ */
+
+
+
+public class Typeerrors {
+  public class TypecheckException extends CheckException {
+  }
+}
+
+
+//----------------------------------------------------------------------
+//	checks/static analysis  Slime programs
+//	----------------------------------------
+//
+//	$Log: not supported by cvs2svn $
+//	
+//---------------------------------------------------------------------
