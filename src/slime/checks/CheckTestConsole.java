f41087340c4d0172d397e7547653d916b372fd99
swprakt <msteffen@ifi.uio.no>
2002-06-19 11:11:32 +0000

    initial

---
+++ src/slime/checks/CheckTestConsole.java
@@ -0,0 +1,30 @@
+/*
+ * CheckTestConsole.java
+ *
+ * Created on 19. Juni 2002, 12:53
+ */
+
+package slime.checks;
+
+/**
+ * @author <a href="mailto:richter@thomas-richter.de">Thomas Richter</a>
+ * @version $Id: CheckTestConsole.java,v 1.1 2002-06-19 11:11:32 swprakt Exp $
+ */
+public class CheckTestConsole {
+
+    /** Creates new CheckTestConsole */
+    public CheckTestConsole() {
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main (String args[]) {
+    }
+
+}
+////////////////////////////////////////////////////////////////
+//
+//	$Log: not supported by cvs2svn $
+//
+////////////////////////////////////////////////////////////////
