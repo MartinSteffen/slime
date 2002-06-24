1da219a652fb5de1763b3f63298564907fb7a278
swprakt <msteffen@ifi.uio.no>
2002-06-24 17:19:55 +0000

    OK

--- src/slime/checks/Typeerrors.java
+++ src/slime/checks/Typeerrors.java
@@ -5,23 +5,27 @@ import java.lang.*;
 import slime.absynt.*;
 
 
+
 /** Exceptions to be thrown by the type checker.
- * The type checker's exceptions are inner classes of
- * this class.
+ * The type checker errors as all specific check exceptions and
+ * thus subclasses of CheckException. They are grouped together
+ * as inner classes.
  *
  * @author Initially provided by Karsten Stahl, Martin Steffen.
- * @version $Id: Typeerrors.java,v 1.3 2002-06-19 23:00:10 swprakt Exp $
+ * @version $Id: Typeerrors.java,v 1.4 2002-06-24 17:19:55 swprakt Exp $
  */
 public class Typeerrors {
-    public class TypecheckException extends CheckException {
-    }
+  public class TypecheckException extends CheckException {
+  }
     
-    public class UnbooleanGuard extends TypecheckException {
-    }
+  public class UnbooleanGuard extends TypecheckException {
+    String explanation = "The guard of a transition must be an expression\n of boolean type. This error indicates the occurrence of a\n well-typed guard-expression, but with a type other than a boolean." ;
+  }
     
-    public class UndeclaredVarible extends TypecheckException{
+  public class UndeclaredVariable extends TypecheckException{
+    String explanation = "Each variable occuring int a program must be\n covered by a type declaration for this variable where the declaration\n is unique";
     }
-    
+  
 }
 
 
@@ -30,6 +34,9 @@ public class Typeerrors {
 //    ----------------------------------------
 //
 //    $Log: not supported by cvs2svn $
+//    Revision 1.3  2002/06/19 23:00:10  swprakt
+//    no message
+//
 //    Revision 1.2  2002/06/14 11:59:10  swprakt
 //    *** empty log message ***
 //
