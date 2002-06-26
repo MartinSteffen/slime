20203355fa7ca6479227899e165c43ff0ce4c56a
swprakt <msteffen@ifi.uio.no>
2002-06-26 15:50:04 +0000

    added input/output flags for variables

--- src/slime/absynt/Variable.java
+++ src/slime/absynt/Variable.java
@@ -7,13 +7,15 @@ import java.io.Serializable;
  * They are very simply implemented as strings
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: Variable.java,v 1.4 2002-06-13 12:34:27 swprakt Exp $	
+ * @version $Id: Variable.java,v 1.5 2002-06-26 15:50:04 swprakt Exp $	
  */
 
 
 public class Variable extends Expr implements Serializable { 
   public String name;
   public Type   type;
+  public boolean inputvar = false;
+  public boolean outputvar = false;
 
   public Variable (String s) {
     name = s;
@@ -23,6 +25,12 @@ public class Variable extends Expr implements Serializable {
   public Variable (String s, Type _t) {
     name = s;
     type  = _t;};
+
+  public Variable (String s, boolean _i, boolean _o) {
+    name = s;
+    type  = null;
+    inputvar = _i;
+    outputvar = _o;};
 
 
   /**
@@ -40,9 +48,12 @@ public class Variable extends Expr implements Serializable {
 //	Abstract syntax for Slime programs
 //	------------------------------------
 //
-//	$Id: Variable.java,v 1.4 2002-06-13 12:34:27 swprakt Exp $
+//	$Id: Variable.java,v 1.5 2002-06-26 15:50:04 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.4  2002/06/13 12:34:27  swprakt
+//	Started to add vistors + typechecks [M. Steffen]
+//	
 //	Revision 1.3  2002/06/12 18:51:58  swprakt
 //	reorganization of the package-structure
 //	
