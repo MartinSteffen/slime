4f9565d5724bddefad047f09c6c795431d9401da
swprakt <msteffen@ifi.uio.no>
2002-07-05 18:34:06 +0000

    *** empty log message ***

---
+++ src/slime/checks/Wellformed.java
@@ -0,0 +1,27 @@
+
+package slime.checks;
+
+import java.lang.*;
+import java.util.*;
+import slime.absynt.*;
+
+
+
+
+/** checking well-formedness for Slime programs
+ *
+ * <p>
+ * @author Initially provided by Martin Steffen and Karsten Stahl.
+ * @version $Id: Wellformed.java,v 1.1 2002-07-05 18:34:06 swprakt Exp $
+ */
+
+public class Wellformed {
+
+
+//----------------------------------------------------------------------
+//    well-formed analysis  for Slime programs
+//    ----------------------------------------
+//
+//    $Log: not supported by cvs2svn $
+//
+//---------------------------------------------------------------------
