985ed6542ae14a29de0e7e881ece999273efb8ca
swprakt <msteffen@ifi.uio.no>
2002-07-07 08:13:30 +0000

    no semantical changes. (I had a printline in there temporarily)
    steffen

--- src/slime/absynt/BoolType.java
+++ src/slime/absynt/BoolType.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
  * Type for booleans.
  * 
  * @author Initially provided by Martin Steffen.
- * @version $Id: BoolType.java,v 1.4 2002-06-24 20:08:10 swprakt Exp $
+ * @version $Id: BoolType.java,v 1.5 2002-07-07 08:13:30 swprakt Exp $
  */
 
 
@@ -18,7 +18,10 @@ public class BoolType  extends Type implements Serializable {
   public Object accept (Visitors.IType ask) throws Exception {
     return ask.forBoolType();
   }
-
+  
+  public boolean equals (Type t) {
+    return  (t instanceof  BoolType);
+  }
 }
 
 
@@ -28,9 +31,15 @@ public class BoolType  extends Type implements Serializable {
 //	Abstract syntax for Slime programs
 //	------------------------------------
 //
-//	$Id: BoolType.java,v 1.4 2002-06-24 20:08:10 swprakt Exp $
+//	$Id: BoolType.java,v 1.5 2002-07-07 08:13:30 swprakt Exp $
 //
 //	$Log: not supported by cvs2svn $
+//	Revision 1.4  2002/06/24 20:08:10  swprakt
+//	Types extended with visitor methods,
+//	also extended is the type ckecker, which is a visitor to the absynt.
+//	
+//	[Steffen]
+//	
 //	Revision 1.3  2002/06/12 18:51:53  swprakt
 //	reorganization of the package-structure
 //	
