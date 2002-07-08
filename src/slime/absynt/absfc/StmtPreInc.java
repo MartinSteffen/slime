279c63ac3502a3fbd71221782ce1aa79b81f331f
swprakt <msteffen@ifi.uio.no>
2002-07-08 20:25:23 +0000

    Added missing post- and pre-increment/decrement classes. (mwe)

---
+++ src/slime/absynt/absfc/StmtPreInc.java
@@ -0,0 +1,69 @@
+package slime.absynt.absfc;
+import java.io.Serializable;
+
+
+/**
+ * An increment-statement, that is postevaluated in the Abstract SFC tree
+ * 
+ * @author initially provided by Marco Wendel.
+ * @version $Id: StmtPreInc.java,v 1.0 2002/06/12 19:53:45 mwe
+ */
+
+
+public class StmtPreInc extends Statement implements Serializable { 
+  private static final long serialVersionUID = Version.getSUID();
+  public slime.absynt.Variable var;
+
+    public String toString() {
+	StringBuffer out = new StringBuffer("");
+	out.append("var := expr;");
+	return out.toString();
+    }
+
+  public StmtPreInc ( slime.absynt.Variable x ) {
+    var        = x;
+    nodetype   = "preinc";
+  }
+ 
+  public StmtPreInc () {
+    var        = null;
+    nodetype   = "preinc";
+    hascontent = false;
+  }
+
+    private void readObject(java.io.ObjectInputStream stream)
+	throws java.io.IOException,java.lang.ClassNotFoundException {
+	// own
+	var        = (slime.absynt.Variable) (stream.readObject());
+	// Absfc
+	depth      = stream.readInt();
+	processed  = stream.readBoolean();
+	hascontent = stream.readBoolean();
+	nodetype   = (String) (stream.readObject());
+	start_step = (slime.absynt.Step) stream.readObject();
+	end_step   = (slime.absynt.Step) stream.readObject();
+	stmt_trans = (slime.absynt.Transition) stream.readObject();
+	// Statement
+	elemNr     = stream.readInt();
+    }
+
+    private void writeObject(java.io.ObjectOutputStream stream)
+	throws java.io.IOException {
+	// own
+	stream.writeObject(  var );
+	// Absfc
+	stream.writeInt(     depth );
+	stream.writeBoolean( processed );
+	stream.writeBoolean( hascontent );
+	stream.writeObject(  nodetype );
+	stream.writeObject(  start_step );
+	stream.writeObject(  end_step );
+	stream.writeObject(  stmt_trans );
+	// Statement
+	stream.writeInt(     elemNr );
+  }
+
+
+
+}
+// ---------------------------------------------------------------------
